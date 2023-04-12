c11ad3d6c5d23c0521b6c4dc031026741f315bd0
Yihao <1070775102@qq.com>
2023-04-13 00:11:19 +0800

    1.0

--- src/Testdemo1/Test.java
+++ src/Testdemo1/Test.java
@@ -2,18 +2,12 @@ package Testdemo1;
 
 public class Test {
     public static void main(String[] args) {
-        Person person =new Person();
-        person.setName("mr.wang");
-        person.setAge(30);
+        Person person =new Person("老王",30);
 
-        Animals animal1=new Dog();
-        animal1.setAge(2);
-        animal1.setColour("白色");
+        Animals animal1=new Dog(2,"white");
         person.keepPet(animal1,"骨头");
 
-        Animals animal2=new Cat();
-        animal2.setAge(77);
-        animal2.setColour("绿色");
+        Animals animal2=new Cat(77,"green");
         person.keepPet(animal2,"🐟");
 
     }
